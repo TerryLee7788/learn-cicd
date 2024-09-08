e4663cd322235c522a6a11e92170e8066bf654d2
TerryLee7788 <k9114658@gmail.com>
2024-09-08 18:19:47 +0800

    fix some error

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,12 @@ const nextConfig = {
   output: "export",
   basePath: "/learn-cicd",
   assetPrefix: "/learn-cicd/",
+  reactStrictMode: true,
+  swcMinify: true,
+  // 启用 SWC
+  experimental: {
+    forceSwcTransforms: true,
+  },
 };
 
 export default nextConfig;
