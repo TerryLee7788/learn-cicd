bc82d1d957381fbfdf95a94331ca0e5306d08c6b
TerryLee7788 <k9114658@gmail.com>
2024-09-08 18:09:32 +0800

    fix test error

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,9 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+  },
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest",
   },
