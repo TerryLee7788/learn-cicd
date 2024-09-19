a16ef3e765911e4109ed57e3aadcf9ad6692237c
TerryLee7788 <k9114658@gmail.com>
2024-09-19 23:50:10 +0800

    調整路徑

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // output: "export",
-  basePath: "/learn-cicd",
-  assetPrefix: "/learn-cicd/",
+  // basePath: "/learn-cicd",
+  // assetPrefix: "/learn-cicd/",
   reactStrictMode: true,
   swcMinify: true,
   // 启用 SWC
