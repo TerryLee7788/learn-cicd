9d4077e7adc35646138bbfd4f5369d2d5b1e93e1
TerryLee7788 <k9114658@gmail.com>
2024-09-19 22:32:00 +0800

    新增 'NEXT_PUBLIC_API_URL' 測試

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,6 +3,7 @@ function AboutPage() {
     <section>
       this is about page
       <p>API_URL: {process.env.API_URL}</p>
+      <p>NEXT_PUBLIC_API_URL: {process.env.NEXT_PUBLIC_API_URL}</p>
     </section>
   );
 }
